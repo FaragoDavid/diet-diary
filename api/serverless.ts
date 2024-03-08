fe5956299ecf1b8b2dbd4d2462d33b5beffe86e3
David Farago <david.farago@emarsys.com>
2024-03-08 17:10:45 +0100

    Fix vercel config

--- api/serverless.ts
+++ api/serverless.ts
@@ -1,11 +1,11 @@
-import fastify from 'fastify';
+import fastify, { FastifyRequest } from 'fastify';
 
-import { server } from '../src/server';
+import { server } from '../src/server.js';
 
 const app = fastify({ logger: false });
 app.register(server, { prefix: '/' });
 
-export default async (req, res) => {
+export default async (req: FastifyRequest, res) => {
   await app.ready();
   app.server.emit('request', req, res);
 };
