bca4a2cd13371977ee1e77586a539a33f4750201
David Farago <david.farago@emarsys.com>
2024-03-13 17:56:13 +0100

    Revert "Adds cors handling"
    This reverts commit 54d48390556e2977f56e73b3c9e0c66fef66a26c.

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@
 import fastify from 'fastify';
 import { fastifyStatic } from '@fastify/static';
 import fastifyFormbody from '@fastify/formbody';
-import cors from '@fastify/cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -15,9 +14,6 @@ const __dirname = path.dirname(__filename);
 const app = fastify({ logger: true });
 
 app.register(fastifyFormbody);
-app.register(cors, {
-  origin: /.*\/public\/.*/,
-});
 app.register(fastifyStatic, {
   root: path.join(__dirname, '../public'),
   prefix: '/public/',
@@ -25,7 +21,7 @@ app.register(fastifyStatic, {
 
 registerRoutes(app);
 
-app.listen({ port: config.port, host: config.host }, (err) => {
+app.listen({ port: config.port, host: config.host}, (err) => {
   if (err) {
     app.log.error(err);
     process.exit(1);
