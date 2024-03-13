54d48390556e2977f56e73b3c9e0c66fef66a26c
David Farago <david.farago@emarsys.com>
2024-03-13 17:41:25 +0100

    Adds cors handling

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@
 import fastify from 'fastify';
 import { fastifyStatic } from '@fastify/static';
 import fastifyFormbody from '@fastify/formbody';
+import cors from '@fastify/cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -14,6 +15,9 @@ const __dirname = path.dirname(__filename);
 const app = fastify({ logger: true });
 
 app.register(fastifyFormbody);
+app.register(cors, {
+  origin: /.*\/public\/.*/,
+});
 app.register(fastifyStatic, {
   root: path.join(__dirname, '../public'),
   prefix: '/public/',
@@ -21,7 +25,7 @@ app.register(fastifyStatic, {
 
 registerRoutes(app);
 
-app.listen({ port: config.port, host: config.host}, (err) => {
+app.listen({ port: config.port, host: config.host }, (err) => {
   if (err) {
     app.log.error(err);
     process.exit(1);
