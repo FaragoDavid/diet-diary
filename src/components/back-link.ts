8ad2bd194dad3befadf4d052ee78b058dc6acea1
David Farago <david.farago@emarsys.com>
2024-04-12 12:17:57 +0200

    Fix back cutton style

--- src/components/back-link.ts
+++ src/components/back-link.ts
@@ -4,7 +4,7 @@ export class BackLink implements BaseComponent {
   async render() {
     return `
       <a href="/dashboard" class="absolute top-2 left-2">
-        <button type="submit" class="btn bg-base-100 border-0 btn-sm">${icons.back}</button>
+        <button type="submit" class="btn-sm">${icons.back}</button>
       </a>
     `;
   }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: ['./src/components/**/*.{html,ts}'],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {
       fontFamily: {
