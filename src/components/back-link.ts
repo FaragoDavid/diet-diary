c153c3c56c44d5a18c972f3c0350dd076dbc28a7
David Farago <david.farago@emarsys.com>
2024-05-01 17:33:47 +0200

    Fixes back button position

--- src/components/back-link.ts
+++ src/components/back-link.ts
@@ -3,7 +3,7 @@ import icons from "../utils/icons.js";
 export class BackLink implements BaseComponent {
   async render() {
     return `
-      <a href="/dashboard" class="absolute top-2 left-2">
+      <a href="/dashboard" class="fixed top-2 left-2 bg-base-200 rounded-lg">
         <button type="submit" class="btn-sm">${icons.back}</button>
       </a>
     `;
