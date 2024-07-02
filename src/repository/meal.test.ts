c1aa3d3ad1b1603c74bf25cf123d7501aef9fd85
David Farago <david.farago@emarsys.com>
2024-07-02 19:53:30 +0200

    Add meal repo tests

---
+++ src/repository/meal.test.ts
@@ -0,0 +1,29 @@
+import { addMeal, deleteMeal, deleteDish, updateDish } from './meal';
+
+describe('Meal Repository', () => {
+  describe('addMeal', () => {
+    it('should add a meal successfully', async () => {
+      
+    });
+  });
+
+  describe('deleteMeal', () => {
+    it('should delete an existing meal', async () => {
+      
+    });
+  });
+
+  describe('addDish', () => {
+    // Tests for adding ingredient, recipe, and error cases
+  });
+
+  describe('deleteDish', () => {
+    it('should delete an existing dish', async () => {
+    });
+  });
+
+  describe('updateDish', () => {
+    it('should update an existing dish with a new amount', async () => {
+    });
+  });
+});
