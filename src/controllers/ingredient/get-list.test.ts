18ff5eb02b213618c47587e4af16ec9465e293e9
david-farago <david.farago@emarsys.com>
2026-01-23 17:25:37 +0100

    Add test for ingredient list filtering and document autocomplete TODO

---
+++ src/controllers/ingredient/get-list.test.ts
@@ -0,0 +1,31 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import getIngredientList from './get-list';
+import * as ingredientRepository from '../../repository/ingredient';
+
+jest.mock('../../repository/ingredient');
+
+describe('ingredient/get-list', () => {
+  let mockRequest: FastifyRequest;
+  let mockReply: FastifyReply;
+
+  beforeEach(() => {
+    mockRequest = {
+      query: { query: 'tomato' },
+    } as FastifyRequest;
+    mockReply = {} as FastifyReply;
+
+    jest.clearAllMocks();
+  });
+
+  it('filters ingredients at database level using query parameter', async () => {
+    const mockIngredients = [
+      { id: '1', name: 'Tomato', calories: 20, carbs: 4, fat: 0 },
+    ];
+
+    (ingredientRepository.fetchIngredients as jest.Mock).mockResolvedValue(mockIngredients);
+
+    await getIngredientList(mockRequest, mockReply);
+
+    expect(ingredientRepository.fetchIngredients).toHaveBeenCalledWith('tomato');
+  });
+});
